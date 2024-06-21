c7b4294c76b7ceeb62216064348492d4533d62f4
KNIGHTW4LKER <samyakaditya0000@gmail.com>
2024-06-21 05:31:38 +0530

    loading screen

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { FaEnvelope, FaPhone, FaInstagram } from 'react-icons/fa';
-
+import email from '../images/email.gif'
 const Contact = () => {
   return (
-    <section>
-      <h2>Contact</h2>
+    <section className='contacts'>
+     <h2 style={{ display: 'flex', alignItems: 'center' }}>
+      Contacts
+      <img src={email} alt="music" style={{ height: '45px', width: '45px', marginLeft: '10px' }} />
+    </h2>
       <p><FaEnvelope /> Email: shahe@example.com</p>
       <p><FaPhone /> Contact Number: +1234567890</p>
       <p><FaInstagram /> Instagram: @shahemusic</p>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { FaTools } from 'react-icons/fa';
-
+import guitar from '../images/electric-guitar.gif'
 const Skills = () => {
   const skills = ['Mix and Master', 'Vocalist, Singer & Songwriter', 'Guitar & Keys', 'FL Studio & Plugins'];
 
   return (
-    <section>
-      <h2>Skills</h2>
+    <section className='skills'>
+      <h2 style={{ display: 'flex', alignItems: 'center' }}>
+      Skills
+      <img src={guitar} alt="music" style={{ height: '45px', width: '45px', marginLeft: '10px' }} />
+    </h2>
       <ul>
         {skills.map((skill, index) => (
           <li key={index}>
